b5e75c1fdd184e3f339c03fe3f958fb8e0e9c5c8
Iain Coffield <icoffiel@nerdery.com>
2017-08-15 20:57:06 -0500

    Unit Test for controller and upping kotlin version.

--- src/main/kotlin/com/nerdery/kotlin/playground/nat/xbox/manufacturer/ManufacturerController.kt
+++ src/main/kotlin/com/nerdery/kotlin/playground/nat/xbox/manufacturer/ManufacturerController.kt
@@ -1,6 +1,7 @@
 package com.nerdery.kotlin.playground.nat.xbox.manufacturer
 
 import com.nerdery.kotlin.playground.nat.xbox.constants.UrlConstants
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 
@@ -15,11 +16,12 @@ class ManufacturerController @Inject constructor(val manufacturerService: Manufa
     fun getOne(@PathVariable id: Long): Manufacturer = manufacturerService.getOne(id)
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun save(@RequestBody manufacturer: Manufacturer): Manufacturer = manufacturerService.save(manufacturer)
 
     @PutMapping("/{id}")
     fun update(@PathVariable id:Long, @RequestBody manufacturer: Manufacturer): Manufacturer = manufacturerService.update(id, manufacturer)
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long): Unit = manufacturerService.delete(id)
+    fun delete(@PathVariable id: Long) = manufacturerService.delete(id)
 }
---
+++ src/test/kotlin/com/nerdery/kotlin/playground/nat/xbox/manufacturer/ManufacturerControllerUnitTest.kt
@@ -0,0 +1,114 @@
+package com.nerdery.kotlin.playground.nat.xbox.manufacturer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.BDDMockito.doNothing
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import javax.inject.Inject
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(ManufacturerController::class)
+class ManufacturerControllerUnitTest {
+
+    @Inject
+    lateinit private var mvc: MockMvc
+
+    @MockBean
+    lateinit private var manufacturerService: ManufacturerService
+
+    private val testManufacturer = Manufacturer(1, "Manufacturer Name")
+
+    @Test
+    fun getAll() {
+        given(manufacturerService.listAll())
+                .willReturn((mutableListOf(testManufacturer)))
+
+        mvc.perform(get("/api/manufacturer"))
+                .andExpect(status().isOk)
+                .andExpect(content().json("""
+                    [
+                        {
+                          "id": 1,
+                          "name": "Manufacturer Name"
+                        }
+                    ]
+                    """))
+    }
+
+    @Test
+    fun getOne() {
+        given(manufacturerService.getOne(1))
+                .willReturn(testManufacturer)
+
+        mvc.perform(get("/api/manufacturer/1"))
+                .andExpect(status().isOk)
+                .andExpect(content().json("""
+                    {
+                        "id": 1,
+                        "name": "Manufacturer Name"
+                    }
+                """))
+    }
+
+    @Test
+    fun save() {
+        given(manufacturerService.save(Manufacturer(null, "Manufacturer Name")))
+                .willReturn(testManufacturer)
+
+        mvc.perform(
+            post("/api/manufacturer")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content("""
+                        {
+                            "name": "Manufacturer Name"
+                        }
+                    """))
+                .andExpect(status().isCreated)
+                .andExpect(content().json("""
+                    {
+                        "id": 1,
+                        "name": "Manufacturer Name"
+                    }
+                """))
+    }
+
+    @Test
+    fun update() {
+        given(manufacturerService.update(1, Manufacturer(null, "Manufacturer Name")))
+                .willReturn(testManufacturer)
+
+        mvc.perform(
+            put("/api/manufacturer/1")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content("""
+                        {
+                            "name": "Manufacturer Name"
+                        }
+                    """))
+                .andExpect(status().isOk)
+                .andExpect(content().json("""
+                    {
+                        "id": 1,
+                        "name": "Manufacturer Name"
+                    }
+                """))
+    }
+
+    @Test
+    fun delete() {
+        doNothing().`when`(manufacturerService).delete(1)
+
+        mvc.perform(delete("/api/manufacturer/1"))
+                .andExpect(status().isOk)
+                .andExpect(content().string(""))
+    }
+
+}
